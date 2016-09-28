0a11e7adb1175a77178379e9699202961833bc6b
Jonathan Reiser <jlr9v@virginia.edu>
2016-09-28 15:29:02 -0400

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,14 @@ var cardFour = "king"
 if (cardOne === cardTwo){
   alert('You found a match!');
 }else{ alert('Sorry, try again.')}
+
+var gameboard = document.getElementById('game-board');
+console.log(gameboard);
+var createCards = function(){
+for (var i = 0; i <= 3; i++){
+  var card = document.createElement("div");
+  card.className = "card";
+  gameboard.appendChild(card);
+};
+}
+createCards();
